698aeabe5d0c4cdbaab0df35e712dd2eafc22fa6
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-05-12 14:44:34 +0200

    construct per il prodotto

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php 
 
-var_dump('Ciao');
+class Product {
+
+//Attr
+ public $category;
+ protected $price;
+
+//Construct
+ public function __construct($category, $price)
+ {
+     $this->category = $category;
+     $this->price = $price;
+
+ }
+}
+
+
+
+
+$rolex = new Product('Watch', 20000);
+
+var_dump($rolex);
