2e7f6a2433ce763caafe928bde4e6780e48eaa5a
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-05-12 17:06:53 +0200

    stampo in pagina html

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 class Product {
 
 //Attr
+ public $name;
  public $category;
  public $img;
  protected $price;
@@ -10,8 +11,9 @@ class Product {
  private $sale;
 
 //Construct
- public function __construct($category, $price, $code,  $img = NULL )
+ public function __construct($name,$category, $price, $code,  $img = NULL )
  {
+     $this->name = $name;
      $this->category = $category;
      $this->price = $price;
      $this->code = $code;
@@ -48,9 +50,9 @@ class Shoe extends Product {
     private $promotionCode;
 
  //Construct
-    public function __construct($category, $price, $code, $avaibleSize, $brand, $img = NULL)
+    public function __construct($name, $category, $price, $code, $avaibleSize, $brand, $img = NULL)
     {
-        parent::__construct($category, $price, $code, $img);
+        parent::__construct($name,$category, $price, $code, $img);
         $this->avaibleSize = $avaibleSize;
         $this->brand = $brand;
     }
@@ -80,22 +82,21 @@ public function getSale() {
 
 
 //Istanze con SuperClasse
-$rolex = new Product('Watch', 20000 , '111-333-444');
-$iPhoneX = new Product('Smartphone', 60, '222-774-333', 'https://asset.mediaw.it/wcsstore/MMCatalogAssetStore/asset/images/14/36/143680.jpg' );
+$rolex = new Product('Rolex GMT Master','Watch', 20000 , '111-333-444');
+$iPhoneX = new Product('iPhone X11','Smartphone', 60, '222-774-333', 'https://asset.mediaw.it/wcsstore/MMCatalogAssetStore/asset/images/14/36/143680.jpg' );
 $rolex->setSale('summer');
 $iPhoneX->setSale('autumn');
-var_dump($rolex);
-var_dump($iPhoneX);
+
+
+$products = [$rolex, $iPhoneX];
 
 //Istanze con SubClasse
-$stanSmith = new Shoe('Shoe', 70 , '111-333-444-777', '44-45' , 'Adidas', 'https://kedoff.net/images/product/large/jenskie-krossovki-adidas-stan-smith-j-m20605-img-4.jpg?v=1583487201');
-$airMax = new Shoe('Shoe', 120, '456-887-095', '42-43-44-45-46', 'Nike');
-$airMax->setPromotion('181-920');
-$airMax->getPromotion();
-echo $airMax->setSale('summer');
-var_dump($stanSmith);
-var_dump($airMax);
+$stanSmith = new Shoe('Stan Smith','Shoe', 70 , '111-333-444-777', '44-45' , 'Adidas', 'https://kedoff.net/images/product/large/jenskie-krossovki-adidas-stan-smith-j-m20605-img-4.jpg?v=1583487201');
+$airMax = new Shoe('Air Max 900','Shoe', 120, '456-887-095', '42-43-44-45-46', 'Nike');
+$stanSmith->setSale('winter');
+$airMax->setSale('summer');
 
+$shoes = [$stanSmith, $airMax];
 ?>
 
 
@@ -106,9 +107,37 @@ var_dump($airMax);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Prodotti</title>
 </head>
 <body>
+ <h1>Products</h1>
+ <section class="products">
+  <?php foreach ($products as $product) { ?>
+  <div class="box">
+  <h2><?php echo $product->name ?></h2>
+  <h3> Categoria: <?php echo $product->category ?></h3>
+  <span>Costo: <?php echo $product->getPrice() ?></span>
+  <img src="<?php echo $product->img ?>" alt="">
+  <p>Sconto: <?php echo $product->getSale() ?>%</p>
+  </div>
+ <?php } ?>
+</section>
+
+<section>
+<?php foreach ($shoes as $shoe) { ?>
+  <div class="box">
+  <h2><?php echo $shoe->name ?></h2>
+  <h3> Categoria: <?php echo $shoe->category ?></h3>
+  <span>Costo: <?php echo $shoe->getPrice() ?></span>
+  <img src="<?php echo $shoe->img ?>" alt="">
+  <p>Sconto: <?php echo $shoe->getSale() ?>%</p>
+  </div>
+ <?php } ?>
+
+</section>
+
+ 
   
     
 </body>
